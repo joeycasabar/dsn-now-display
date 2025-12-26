bb65c03ec9db4b4bdadcb18cfef861f7fb642939
Joey C <joeycasabar@gmail.com>
2025-12-26 02:12:28 -0800

    line animation test

---
+++ src/line_test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import time
+from PIL import Image, ImageDraw, ImageFont
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+MATRIX_WIDTH = 32
+
+LINE_BRIGHTNESS = 64
+
+UP_LINES = [
+    (1, 1),
+    (16, 8),
+    (1, 16),
+    (16, 24),
+    (1, 30)
+]
+
+DOWN_LINES = [
+    (12, 1),
+    (4, 4),
+    (12, 7),
+    (4, 10),
+    (12, 13),
+    (4, 16),
+    (12, 19),
+    (4, 22),
+    (12, 25),
+    (4, 28),
+    (12, 32)
+]
+
+line_color = (LINE_BRIGHTNESS, LINE_BRIGHTNESS, LINE_BRIGHTNESS)
+
+options = RGBMatrixOptions()
+
+options.hardware_mapping = 'adafruit-hat'
+options.rows = 16
+options.cols = MATRIX_WIDTH
+
+matrix = RGBMatrix(options=options)
+
+image = Image.new("RGB", (16, 32), color="black")
+# 2. Create a drawing context (object)
+draw = ImageDraw.Draw(image)
+
+next_text = time.time()
+
+while True:
+    for x_pos in range(32, -32, -1):
+        draw.rectangle((1, 1, 16, 32), fill='black')
+        draw.line(DOWN_LINES, fill=line_color)
+        rotated_image = image.rotate(270, expand=True)
+        matrix.SetImage(rotated_image, offset_x=x_pos, unsafe=False)
+        # matrix.SetImage(rotated_image, offset_x=x_pos -
+        #                 MATRIX_WIDTH, unsafe=False)
+        time.sleep(0.05)
+    for x_pos in range(-32, 32):
+        print(x_pos)
+        draw.rectangle((1, 1, 16, 32), fill='black')
+        draw.line(UP_LINES, fill=line_color)
+        rotated_image = image.rotate(270, expand=True)
+        matrix.SetImage(rotated_image, offset_x=x_pos, unsafe=False)
+        # matrix.SetImage(rotated_image, offset_x=x_pos +
+        #                 MATRIX_WIDTH, unsafe=False)
+        time.sleep(0.1)
