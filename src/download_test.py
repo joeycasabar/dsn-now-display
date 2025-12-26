cad8ccda8cbcbe1528c6eb98ee5d443f64926616
Joey C <joeycasabar@gmail.com>
2025-12-26 00:49:34 -0800

    added automatic renaming when downloading new data

--- src/download_test.py
+++ src/download_test.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 
 import requests
+import dsn_util
 
+import os
+
+filename = 'dsn.xml'
 url = "https://eyes.nasa.gov/dsn/data/dsn.xml"  # Replace with your XML URL
 response = requests.get(url)
 
 # Ensure the request was successful
 response.raise_for_status()
 
+try:
+    xml_ts = f"{dsn_util.get_ts_from_xml(filename):.0f}"
+    os.rename(filename, 'dsn_' + xml_ts + '.xml')
+
+except FileNotFoundError:
+    print(f"Error: The file '{filename}' was not found.")
+
+
 # Open a local file in binary write mode ('wb') and save the content
-with open('dsn.xml', 'wb') as file:
+with open(filename, 'wb') as file:
     file.write(response.content)
 
-print(f"Successfully saved XML content to dsn.xml")
+print(f"Successfully saved XML content to {filename}")
